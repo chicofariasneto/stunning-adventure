b5b4366a6622da61d4bfc2a7dae8a1ded6e7bcb3
chicofariasneto <chicofariasneto@gmail.com>
2021-01-17 21:32:15 -0300

    update port api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT;
+const port = 5000;
 
 app.use(bodyParser.json());
 
